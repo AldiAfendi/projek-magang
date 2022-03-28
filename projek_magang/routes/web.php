192513ca894a50d7459a1b9348a65dddda56cb38
AldiAfendi <aldiafendi78@gmail.com>
2022-03-28 22:13:57 +0700

    p

--- projek_magang/routes/web.php
+++ projek_magang/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controller\LoginController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/login/login16', [LoginController::class, 'index']);
